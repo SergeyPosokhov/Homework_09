58a96d262afaa3ea30c782dbbbd9b1ebc9c335d4
Sergey Posokhov <posokhov_pit@mail.ru>
2022-11-15 10:08:15 +0300

    feat: add Homework 09 Task 66

---
+++ Homework09_Task066/Program.cs
@@ -0,0 +1,19 @@
+﻿// //Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// //в промежутке от M до N.
+
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sumRec(int m, int n)
+{
+    if (n > m)
+        return n + sumRec(m, n - 1);
+    return m;
+}
+
+int sum = sumRec(m, n);
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел от {m} до {n} равна {sum}"); 
