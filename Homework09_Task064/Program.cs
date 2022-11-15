e1401278e7db0645f10aa1dc8ecd221f7ee8af7e
Sergey Posokhov <posokhov_pit@mail.ru>
2022-11-15 10:05:43 +0300

    init: init add Homework 09 Task 64

---
+++ Homework09_Task064/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+Console.Write("Введите целое число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string SumRec (int n)
+{
+    
+    if (n == 0) return "";
+    return Convert.ToString(n) + " " + SumRec (n - 1);
+    
+}
+   
+Console.WriteLine();
+Console.WriteLine($"{SumRec(n)}");
